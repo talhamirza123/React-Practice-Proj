ec586019b8673946d51823ee7a5d778e31f9c066
Talha Mirza <mirzatalha178@gmail.com>
2020-12-19 22:39:45 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const person = 
+// {
+//     name: "Talha",
+//     walk()
+//     {
+//         console.log(this);
+//     }
+    
+// };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// person.walk();
+
+// const walk = person.walk.bind(person);
+// walk(); 
+
+
+//Arrow Functions
+
+// const square = number => number * number;
+// console.log(square(7));
+
+//Arrow Functions
+
+// const jobs = 
+// [
+//    { id: 1, isactive: true },
+//    { id: 2, isactive: true },
+//    { id: 3, isactive: false },
+
+// ];
+   
+//    const activejobs =  jobs.filter(function(job) {   return  job.isactive  });
+//    const activejobs =  jobs.filter(function(job) {   return  job.isactive  });
+
+
+//Arrow function and this
+
+
+// const person = 
+// {
+    
+    
+//     talk()
+//     {
+        
+//         var self = this;
+        
+//         setTimeout
+        
+//             (function () 
+//                 {
+//             console.log("self", self);
+//         }, 1000 );
+//     }
+// }; 
+    
+
+// person.talk();
+
+
+
+
+// const colors = ['green', 'red', 'blue'];
+
+// const items = colors.map(color => `<li>${color}</li>`);
+
+
+
+// console.log(items);
+
+// const address = 
+// {
+//     street: '',
+//     city: '',
+//     country: '',
+    
+    
+// }
+// const { street, city, country } = address,
+
+// const first = [1, 2 , 3];
+
+// const clone = [...first];
+
+// console.log(first);
+// console.log(clone);   
+    
+const first = {name: "Talha"}
+const second = {ob: 'Developer'};
+
+const combined = {...first, ...second}
+console.log(combined);
